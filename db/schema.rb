bc1c703d6cd9222dc4b74a041d05a5e2369c5219
sourabhkeshri <sourabhkeshri93@gmail.com>
2019-08-12 22:54:48 +0530

    create employee model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_054155) do
+ActiveRecord::Schema.define(version: 2019_08_12_172222) do
+
+  create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
